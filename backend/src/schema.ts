1d2253cd7540743ae7be84e8878e59fec5b1e359
sharafeddeen <ad17asha@uwcad.it>
2024-02-08 18:36:03 -0600

    integrated apollo with express - also separated schema (schema.ts) from server file (index.ts)

---
+++ backend/src/schema.ts
@@ -0,0 +1,47 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config({ path: 'src/db-credentials.env' });
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xw40vvf.mongodb.net/?retryWrites=true&w=majority`
+
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+
+  type Dish {
+    id: Int
+    name: String
+    price: Float
+    ingredients: String
+    calories: Int
+    description: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    dishes: [Dish]
+  }
+`;
+
+const resolvers = {
+    Query: {
+      dishes: async () => {
+        // Connect to MongoDB and retrieve dishes
+        const client = await MongoClient.connect(uri);
+        const db = client.db("restaurant-db"); // Replace with your database name
+        const dishesCollection = db.collection("dishes");
+        const dishesData = await dishesCollection.find().toArray(); // Fetch all dishes
+
+        // Close the connection
+        await client.close();
+
+        return dishesData;
+      },
+    },
+};
+
+export { typeDefs, resolvers };
