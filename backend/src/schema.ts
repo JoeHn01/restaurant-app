2591a0ddc777a411a34c9eb62ec9360e3bebed12
JoeHn01 <yousef.hanafi2001@gmail.com>
2024-02-14 17:00:00 -0700

    Separated connect/fetch logic and added fetching by id

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config({ path: 'src/db-credentials.env' });
 
@@ -8,63 +8,118 @@ const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERNAME}
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = `#graphql
+  # scalar DateTime
 
-  type Dish {
+  # type Dish {
+  #   _id: ID!
+  #   name: String
+  #   price: Float
+  #   category: String
+  #   ingredients: String
+  #   calories: Int
+  #   description: String
+  # }
+
+  type User {
     _id: ID!
-    name: String
-    price: Float
-    category: String
-    ingredients: String
-    calories: Int
-    description: String
+    fullname: String!
+    username: String!
+    email: String!
+    role: String!
+  }
+
+  type Item {
+    _id: ID!
+    name: String!
+    description: String!
+    price: Float!
+    category_id: ID!
   }
 
   type Category {
     _id: ID!
-    name: String
+    name: String!
     description: String
+    items: [ID]
+  }
+
+  type Order {
+    _id: ID!
+    type: String!   # (e.g. delivery, pickup, dine-in)
+    user_id: ID
+    total: Float!
+    # time: DateTime!  # need to define scalar type (DateTime)
+    status: String!
+    order_items: [ID!]
+  }
+
+  type OrderItem {
+    order_id: ID!
+    item_id: ID!
+    quantity: Int!
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    dishes: [Dish]
+    # dishes: [Dish]
+    users: [User]
+    user(id: ID!): User
+
+    items: [Item]
+    item(id: ID!): Item
+
     categories: [Category]
+    category(id: ID!): Category
+
+    orders: [Order]
+    order(id: ID!): Order
   }
 `;
 
-async function getData(collection: string) {
-
+async function connectToDB(collection: string): Promise<{ client: MongoClient; collectionObj: any }> {
   try {
-    // Connect to the MongoDB server
     const client = await MongoClient.connect(uri);
-
-    // Get the database
     const db = client.db("restaurant-app-db");
-
-    // Access the specified collection
     const collectionObj = db.collection(collection);
 
-    // Fetch all documents from the collection
-    const data = await collectionObj.find().toArray();
-
-    // Close the connection
-    await client.close();
-
-    return data;
-
+    return { client, collectionObj };
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    throw error; // Re-throw the error for handling in the calling code
+    throw error;
   }
 }
 
+async function getAll<T>(collection: string): Promise<T[]> {
+  const { client, collectionObj } = await connectToDB(collection);
+  const data = await collectionObj.find().toArray();
+  await client.close();
+  return data;
+}
+
+async function getById<T>(collection: string, id: string): Promise<T> {
+  const { client, collectionObj } = await connectToDB(collection);
+  const data = await collectionObj.findOne({ _id: new ObjectId(id) });
+  await client.close();
+  return data;
+}
+
 const resolvers = {
-    Query: {
-      dishes: () => getData("dishes"),
-      categories: () => getData("category"),
-    },
+  Query: {
+    // dishes: () => getData("dishes"),
+    users: () => getAll("user"),
+    user: (_: any, args: any ) => getById("user", args.id),
+
+    items: () => getAll("item"),
+    item: (_: any, args: any ) => getById("item", args.id),
+
+    categories: () => getAll("category"),
+    category: (_: any, args: any ) => getById("category", args.id),
+
+    orders: () => getAll("order"),
+    order: (_: any, args: any ) => getById("order", args.id),
+  },
 };
 
 export { typeDefs, resolvers };
