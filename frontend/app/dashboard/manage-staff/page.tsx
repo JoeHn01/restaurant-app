68e3b6e494827a5bfad676299f299ace6eb844a3
sharafeddeen <ad17asha@uwcad.it>
2024-02-27 22:38:21 -0600

    renamed more dashboard tabs

--- frontend/app/dashboard/manage-staff/page.tsx
+++ frontend/app/dashboard/manage-staff/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CustomersPage: React.FC = () => {
   return (
     <div>
-        <h1>Customers</h1>
+        <h1>Manage Staff</h1>
         {/* Add your view menu content here */}
     </div>
   );
--- frontend/app/dashboard/past-orders/page.tsx
+++ frontend/app/dashboard/past-orders/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const OrdersPage: React.FC = () => {
   return (
     <div>
-        <h1>Orders</h1>
+        <h1>Past Orders</h1>
         {/* Add your view menu content here */}
     </div>
   );
