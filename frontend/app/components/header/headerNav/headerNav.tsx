94f010e2022f9165af06eb91a6bdcfaa67de6984
JoeHn01 <yousef.hanafi2001@gmail.com>
2024-04-23 14:27:46 -0600

    Moved Nav media queries and cleaned the component code

--- frontend/app/components/header/headerNav/headerNav.tsx
+++ frontend/app/components/header/headerNav/headerNav.tsx
@@ -4,42 +4,32 @@ import styles from "./headerNav.module.css"
 import React from 'react'
 
 const HeaderNav = () => {
-    const handleNavClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-        event.preventDefault(); // Prevent default jumping behavior
-        const targetSectionId = event.currentTarget.textContent?.toLowerCase(); // Extract section ID (lowercase)
-        if (targetSectionId) {
-          const targetElement = document.getElementById(targetSectionId);
-          if (targetElement) {
-            // Smooth scrolling to target element
-            targetElement.scrollIntoView({ behavior: 'smooth' });
-          } else {
-            // Handle case where element might not exist
-            console.warn(`Element with ID '${targetSectionId}' not found.`);
-          }
-        }
-    };
+  const navItems = ['Home', 'About', 'Menu', 'Gallery', 'Contact', 'Footer'];
+
+  const handleNavClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault(); // Prevent default jumping behavior
+    const targetSectionId = event.currentTarget.textContent?.toLowerCase(); // Extract section ID (lowercase)
+    if (targetSectionId) {
+      const targetElement = document.getElementById(targetSectionId);
+      if (targetElement) {
+        // Smooth scrolling to target element
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      } else {
+        // Handle case where element might not exist
+        console.warn(`Element with ID '${targetSectionId}' not found.`);
+      }
+    }
+  };
+
   return (
     <nav>
-    <ul className={styles.navList}>
-      <li className={styles.navItem}>
-        <a className={styles.navLink} onClick={handleNavClick}>Home</a>
-      </li>
-      <li className={styles.navItem}>
-        <a className={styles.navLink} onClick={handleNavClick}>About</a>
-      </li>
-      <li className={styles.navItem}>
-        <a className={styles.navLink} onClick={handleNavClick}>Menu</a>
-      </li>
-      <li className={styles.navItem}>
-        <a className={styles.navLink} onClick={handleNavClick}>Gallery</a>
-      </li>
-      <li className={styles.navItem}>
-        <a className={styles.navLink} onClick={handleNavClick}>Contact</a>
-      </li>
-      <li className={styles.navItem}>
-        <a className={styles.navLink} onClick={handleNavClick}>Footer</a>
-      </li>
-    </ul>
+      <ul className={styles.navList}>
+        {navItems.map((item, index) => (
+          <li key={index} className={styles.navItem}>
+            <a className={styles.navLink} onClick={handleNavClick}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
